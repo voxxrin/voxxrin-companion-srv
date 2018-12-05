f77b10297a1bd57cb125f063bab43e444a7ebac9
Elian ORIOU <eoriou@gmail.com>
2018-12-05 23:56:12 +0100

    remove useless files

--- srv/start.bat
+++
@@ -1,2 +0,0 @@
-set APP_OPTIONS="-Doauth.twitter.apiSecret=_ -Doauth.twitter.apiKey=_ -Dws.ionic.app.privateKey=_ -Dws.ionic.app.id=_ -Doauth.secrets.token=_"
-java -cp "target/restx/classes;target/dependency/*" -Drestx.app.package=voxxrin2 -Drestx.mode=prod %APP_OPTIONS% %VM_OPTIONS%  voxxrin2.AppServer
